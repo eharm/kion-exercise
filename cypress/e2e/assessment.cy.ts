2954cdccb81a00cf6f6f4433d0d5681c84eeb237
Evan Harmony <evan_harmony@hotmail.com>
2023-01-27 15:20:41 -0500

    fixing minor formatting issues

--- cypress/e2e/assessment.cy.ts
+++ cypress/e2e/assessment.cy.ts
@@ -28,21 +28,20 @@ describe('Kion assessment', () => {
       cy.visit(searchUrl);
       hotelSearch.checkUrl();
       hotelSearch.addChildren(8, 10);
-   })
+   });
 
    it('Test Case 3', () => {
       home.selectTab('Flights');
       home.verifyFlightDefaults();
       home.selectClass('first class');
-   })
+   });
 
    it('Test Case 4', () => {
       home.selectTab('Flights');
       home.verifyOneWay();
-   })
+   });
 
-   it('Test Case 5', () =>
-   {
+   it('Test Case 5', () => {
       home.selectTab('Stays', 'Flights', 'Cars', 'Packages', 'Things to do', 'Cruises')
-   })
+   });
 });
